261c2543796440acf40652bfac07655ff2682bef
Jonathan K <temidee11@yahoo.co.uk>
2025-10-20 19:17:01 +0100

    add insertion sort algorithm

---
+++ algorithms/insertion-sort.js
@@ -0,0 +1,10 @@
+export default function insertionSort(arr) {
+  for (let i = 0; i < arr.length - 1; ++i)
+    for (let j = i + 1; j > 0; --j) {
+      if (arr[j] < arr[j - 1]) {
+        const temp = arr[j]
+        arr[j] = arr[j - 1]
+        arr[j - 1] = temp
+      }
+    }
+}
